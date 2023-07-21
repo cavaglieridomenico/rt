7e361d0ac3daaa95c3bbd0c3436b33475ae5c777
cavaglieridomenico <cavaglieridomenico@gmail.com>
2023-07-21 08:59:46 +0200

    Add the component structure

--- hangman/src/App.tsx
+++ hangman/src/App.tsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import words from "./wordList.json";
+import { HangmanDrawing } from "./HangmanDrawing";
+import { HangmanWord } from "./HangmanWord";
+import { Keyboard } from "./Keyboad";
 
 function App() {
   const [wordToGuess /*, setWordGuess*/] = useState(
@@ -19,6 +22,9 @@ function App() {
       }}
     >
       <div style={{ fontSize: "2rem", textAlign: "center" }}>Lose Win</div>
+      <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard />
     </div>
   );
 }
---
+++ hangman/src/HangmanWord.tsx
@@ -0,0 +1,3 @@
+export const HangmanWord = () => {
+  return <div>HangmanWord</div>;
+};
