8da3eebb05abd6f1cb04128b199ec539bdf03213
cavaglieridomenico@gmail.com <cavaglieridomenico@gmail.com>
2023-07-23 19:41:43 +0200

    Add HangmanWord component without logic

--- hangman/src/HangmanWord.tsx
+++ hangman/src/HangmanWord.tsx
@@ -1,3 +1,32 @@
 export const HangmanWord = () => {
-  return <div>HangmanWord</div>;
+  const word = "test";
+  const guessedLetter: string[] = [];
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: ".25em",
+        fontSize: "6rem",
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        fontFamily: "monospace",
+      }}
+    >
+      {word.split("").map((letter, index) => {
+        return (
+          <div
+            key={index}
+            style={{
+              borderBottom: ".1em solid black",
+              width: ".5em",
+              height: ".7em",
+            }}
+          >
+            {guessedLetter.includes(letter) && <span>{letter}</span>}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
