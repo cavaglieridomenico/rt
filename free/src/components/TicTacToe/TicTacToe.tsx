93dd2a08fc323b721a5547c9c432a0e8b3fc4e29
cavaglieridomenico@gmail.com <cavaglieridomenico@gmail.com>
2023-11-19 18:23:24 +0100

    Add various updates

--- free/src/components/TicTacToe/TicTacToe.tsx
+++ free/src/components/TicTacToe/TicTacToe.tsx
@@ -6,19 +6,19 @@ import Board from "./Board";
 import { TictactoeValues } from "../../typings/tic-tac-toe.d";
 
 const TicTacToe = () => {
-  const [isTurnX, setIsTurnX] = useState(true);
-  // const [boardValues, setBoardValues] = useState<any>(Array(9).fill(null));
-  // const [movementList, setMovementList] = useState<TictactoeValues[]>([
-  //   [null, null, null, null, null, null, null, null, null],
-  // ]);
   const [history, setHistory] = useState<TictactoeValues[]>([
     [null, null, null, null, null, null, null, null, null],
   ]);
   const [historyIndex, setHistoryIndex] = useState(history.length - 1);
   let status: string;
+  let isTurnX = historyIndex % 2 === 0;
 
   useEffect(() => {
-    console.log(historyIndex);
+    console.log("history:", history);
+  }, [history]);
+
+  useEffect(() => {
+    console.log("historyIndex:", historyIndex);
   }, [historyIndex]);
 
   if (getTictactoeWinner(history[historyIndex])) {
@@ -35,31 +35,24 @@ const TicTacToe = () => {
       return;
     setHistory((prevState: any) => {
       let newMovementList = [...prevState];
-      newMovementList.splice(historyIndex + 1, newMovementList.length - index);
-      console.log("#############");
-      console.log(historyIndex);
-      console.log(newMovementList);
+      newMovementList.splice(
+        historyIndex + 1,
+        newMovementList.length - historyIndex
+      );
       setHistoryIndex(newMovementList.length - 1);
       return newMovementList;
     });
     let newList = [...history[historyIndex]];
     if (isTurnX) {
       newList[index] = "X";
-      setIsTurnX(false);
     } else if (!isTurnX) {
       newList[index] = "O";
-      setIsTurnX(true);
     } else {
       return;
     }
-    console.log(newList);
     setHistory((prevState: any) => {
       let newMovementList = [...prevState];
-      newMovementList.splice(historyIndex + 1, newMovementList.length - index);
-      console.log("..................");
-      console.log(newMovementList);
       newMovementList.push(newList);
-      console.log(newMovementList);
       setHistoryIndex(newMovementList.length - 1);
       return newMovementList;
     });
