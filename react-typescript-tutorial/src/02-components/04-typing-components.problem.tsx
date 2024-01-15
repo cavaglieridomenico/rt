2110d9abfa31b5b700fd0dfb63b53f01822ffb1d
cavaglieridomenico@gmail.com <cavaglieridomenico@gmail.com>
2024-01-15 09:00:05 +0100

    RC Tutorial initial commit

---
+++ react-typescript-tutorial/src/02-components/04-typing-components.problem.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+type ButtonProps = {
+  className: string;
+};
+
+export const Button = ({ className }: ButtonProps) => {
+  return <button className={className}></button>;
+};
+
+const Parent = () => {
+  return (
+    <>
+      {/* @ts-expect-error */}
+      <Button></Button>
+
+      <Button className="my-class"></Button>
+    </>
+  );
+};
