65c67079552573cca255ffc95a6dcfe290230b60
cavaglieridomenico@gmail.com <cavaglieridomenico@gmail.com>
2024-01-21 20:14:21 +0100

    Add various updates

--- youtube/src/layout/Sidebar.tsx
+++ youtube/src/layout/Sidebar.tsx
@@ -3,16 +3,19 @@ import SmallSidebarItem from "../component/SmallSidebarItem";
 
 const Sidebar = () => {
   return (
-    <div>
-      <SmallSidebarItem Icon={Home} url="/" title="Home" />
-      <SmallSidebarItem Icon={Repeat} url="/shorts" title="Shorts" />
-      <SmallSidebarItem
-        Icon={Clapperboard}
-        url="/subscription"
-        title="Subscription"
-      />
-      <SmallSidebarItem Icon={Library} url="/library" title="Library" />
-    </div>
+    <>
+      <aside className="sticky top-0 overflow-y-auto scrollbar-hidden pb-4 flex flex-col ml-1 lg:hidden">
+        <SmallSidebarItem Icon={Home} url="/" title="Home" />
+        <SmallSidebarItem Icon={Repeat} url="/shorts" title="Shorts" />
+        <SmallSidebarItem
+          Icon={Clapperboard}
+          url="/subscription"
+          title="Subscription"
+        />
+        <SmallSidebarItem Icon={Library} url="/library" title="Library" />
+      </aside>
+      <aside className="w-56 lg:sticky absolute top-0 overflow-y-auto scrollbar-hidden pb-4 flex flex-col gap-2 px-2"></aside>
+    </>
   );
 };
 
