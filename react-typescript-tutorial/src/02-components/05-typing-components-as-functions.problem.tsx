bc4ef1a30f5036a02862c08a7ee21dc8292e7714
cavaglieridomenico@gmail.com <cavaglieridomenico@gmail.com>
2024-01-21 19:25:26 +0100

    Add various updates

--- react-typescript-tutorial/src/02-components/05-typing-components-as-functions.problem.tsx
+++ react-typescript-tutorial/src/02-components/05-typing-components-as-functions.problem.tsx
@@ -1,20 +1,23 @@
-import React from "react";
-
-interface Props {
+interface ButtonProps {
   className: string;
 }
 
-/* @ts-expect-error */
-export const Button = (props: Props) => {
-  return {
-    ohDear: "123",
-  };
+// you can choose annotate the return type so an error is raised if you accidentally return some other type
+export const Button = ({ className }: ButtonProps): JSX.Element => {
+  return <div className={className}></div>;
+};
+
+// Alternatively, you can use `React.FunctionComponent` (or `React.FC`), if you prefer.
+// With latest React types and TypeScript 5.1. it's mostly a stylistic choice, otherwise discouraged.
+export const Button2: React.FC<ButtonProps> = ({ className }: ButtonProps) => {
+  return <div className={className}></div>;
 };
 
 const Parent = () => {
   return (
     <>
-      <Button className="my-class"></Button>
+      <Button className="my-class" />
+      <Button2 className="my-class2" />
     </>
   );
 };
